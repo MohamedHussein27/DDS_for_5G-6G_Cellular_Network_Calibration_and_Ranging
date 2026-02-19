cab2ce807c36ca3b55e2d715b87e358d2a6bd7e0
Mohamed Hussein <Mohamed_Hussein2100924@outlook.com>
2026-02-19 16:04:16 +0200

    update test with reset phase
    had a problem that reset phase doesn't excute immediately, it waits for the finishing of first transaction

--- Verification/Assignments_Mohamed_Hussein/Task_3/alu_uvm_env/alu_test.sv
+++ Verification/Assignments_Mohamed_Hussein/Task_3/alu_uvm_env/alu_test.sv
@@ -16,8 +16,9 @@ package alu_test_pkg;
     class alu_base_test extends uvm_test;
         `uvm_component_utils(alu_base_test)
 
-        alu_env        env;
-        alu_config_obj alu_cfg;
+        alu_env            env;
+        alu_config_obj     alu_cfg;
+        alu_reset_sequence reset_seq;
 
         function new(string name = "alu_base_test", uvm_component parent = null);
             super.new(name, parent);
@@ -26,8 +27,9 @@ package alu_test_pkg;
         function void build_phase(uvm_phase phase);
             super.build_phase(phase);
 
-            env     = alu_env::type_id::create("env", this);
-            alu_cfg = alu_config_obj::type_id::create("alu_cfg");
+            env       = alu_env::type_id::create("env", this);
+            alu_cfg   = alu_config_obj::type_id::create("alu_cfg");
+            reset_seq = alu_reset_sequence::type_id::create("reset_seq");
 
             if (!uvm_config_db #(virtual alu_if)::get(this,"","alu_V", alu_cfg.alu_vif))
             `uvm_fatal("build_phase", "Main interface not found");
@@ -35,18 +37,32 @@ package alu_test_pkg;
             uvm_config_db #(alu_config_obj)::set(this,"*","CFG", alu_cfg);
         endfunction
 
+
+        // (runs automatically before main_phase)
+        task reset_phase(uvm_phase phase);
+            super.reset_phase(phase);
+
+            phase.raise_objection(this);
+
+            `uvm_info("RESET_PHASE", "Starting Reset Sequence", UVM_LOW)
+            reset_seq.start(env.agt.sqr);
+            `uvm_info("RESET_PHASE", "Reset Completed", UVM_LOW)
+
+            // debug when it excutes
+            `uvm_info("RESET_PHASE", $sformatf("Time = %0t", $time), UVM_LOW) 
+
+            phase.drop_objection(this);
+        endtask
+
     endclass
 
 
+
     class alu_add_xor_test extends alu_base_test;
-        `uvm_component_utils(alu_add_xor_test) 
-        
-        // Sequence Handles
-        alu_reset_sequence reset_seq;
-        
-        // Specific Directed Sequences based on your imports
-        alu_add_sequence   add_seq;
-        alu_xor_sequence   xor_seq;
+        `uvm_component_utils(alu_add_xor_test)
+
+        alu_add_sequence add_seq;
+        alu_xor_sequence xor_seq;
 
         function new(string name = "alu_add_xor_test", uvm_component parent = null);
             super.new(name, parent);
@@ -55,47 +71,26 @@ package alu_test_pkg;
         function void build_phase(uvm_phase phase);
             super.build_phase(phase);
 
-            // Sequence Creation
-            reset_seq = alu_reset_sequence::type_id::create("reset_seq");
-            
-            // Create specific directed sequences
-            add_seq   = alu_add_sequence::type_id::create("add_seq");
-            xor_seq   = alu_xor_sequence::type_id::create("xor_seq");
-
-            // Config DB gets
-            if (!uvm_config_db #(virtual alu_if)::get(this,"","alu_V", alu_cfg.alu_vif))
-                `uvm_fatal("build_phase", "Main interface not found");
-
-            uvm_config_db #(alu_config_obj)::set(this,"*","CFG", alu_cfg);
+            add_seq = alu_add_sequence::type_id::create("add_seq");
+            xor_seq = alu_xor_sequence::type_id::create("xor_seq");
         endfunction
 
+
         task run_phase(uvm_phase phase);
-            super.run_phase(phase);
             phase.raise_objection(this);
 
-            // 1. Initial Reset to start the simulation
-            `uvm_info("run_phase", "Initial reset asserted", UVM_LOW)
-            reset_seq.start(env.agt.sqr);
-            `uvm_info("run_phase", "Initial reset deasserted", UVM_LOW)
-
-            // 2. Executing Directed Sequences First
-            `uvm_info("run_phase", "Executing Directed Test Battery", UVM_LOW)
-
-            `uvm_info("run_phase", "Starting ADD Sequence", UVM_LOW)
             add_seq.start(env.agt.sqr);
-
-            `uvm_info("run_phase", "Starting XOR Sequence", UVM_LOW)
             xor_seq.start(env.agt.sqr);
 
             phase.drop_objection(this);
         endtask
+
     endclass
+
 
     class alu_and_or_test extends alu_base_test;
         `uvm_component_utils(alu_and_or_test)    
         
-        // Sequence Handles
-        alu_reset_sequence reset_seq;
         
         // Specific Directed Sequences based on your imports
         alu_and_sequence   and_seq;
@@ -109,7 +104,6 @@ package alu_test_pkg;
             super.build_phase(phase);
 
             // Sequence Creation
-            reset_seq = alu_reset_sequence::type_id::create("reset_seq");
             
             and_seq   = alu_and_sequence::type_id::create("and_seq");
             or_seq    = alu_or_sequence::type_id::create("or_seq");
@@ -118,11 +112,6 @@ package alu_test_pkg;
         task run_phase(uvm_phase phase);
             super.run_phase(phase);
             phase.raise_objection(this);
-
-            // 1. Initial Reset to start the simulation
-            `uvm_info("run_phase", "Initial reset asserted", UVM_LOW)
-            reset_seq.start(env.agt.sqr);
-            `uvm_info("run_phase", "Initial reset deasserted", UVM_LOW)
 
             `uvm_info("run_phase", "Starting AND Sequence", UVM_LOW)
             and_seq.start(env.agt.sqr);
@@ -137,7 +126,6 @@ package alu_test_pkg;
     class alu_rand_test extends alu_base_test;
         `uvm_component_utils(alu_rand_test)
 
-        alu_reset_sequence reset_seq;
         alu_main_sequence  main_seq;
 
         function new(string name = "alu_rand_test", uvm_component parent = null);
@@ -147,14 +135,12 @@ package alu_test_pkg;
         function void build_phase(uvm_phase phase);
             super.build_phase(phase);
 
-            reset_seq = alu_reset_sequence::type_id::create("reset_seq");
             main_seq  = alu_main_sequence::type_id::create("main_seq");
         endfunction
 
         task run_phase(uvm_phase phase);
             phase.raise_objection(this);
 
-            reset_seq.start(env.agt.sqr);
             main_seq.start(env.agt.sqr);
 
             phase.drop_objection(this);
