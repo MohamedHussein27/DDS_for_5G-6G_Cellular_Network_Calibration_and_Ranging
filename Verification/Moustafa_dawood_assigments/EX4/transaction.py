c9cc8977ef2b0f633d8283c44b5ee8d7e7da8709
Dawood901 <132154158+Dawood901@users.noreply.github.com>
2026-02-26 04:48:11 +0200

    Deleting EX4 and renamed to Task_4 for consistency naming

--- Verification/Moustafa_dawood_assigments/EX4/transaction.py
+++
@@ -1,7 +0,0 @@
-class Transaction:
-    def __init__(self, tid, data):
-        self.tid = tid
-        self.data = data
-
-    def __repr__(self):
-        return f"Transaction(TID={self.tid}, DATA={self.data})"
