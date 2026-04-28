6cf3009ddd2583e75d241adcdc89bfcbc1d1f432
Hossam <ahmedalgamal789@yahoo.com>
2026-04-28 20:37:50 +0300

    top_sequencer added

---
+++ Verification/Tx_Path_Verification/Tx_Path_env/Agents/TOP/top_sequencer.py
@@ -0,0 +1,11 @@
+""""""
+import cocotb 
+from cocotb.triggers import * 
+from cocotb.clock    import Clock
+from cocotb_coverage.crv import *
+from pyuvm import * 
+import pyuvm
+import logging
+
+class top_sequencer(uvm_sequencer):
+    pass
