4320cbe25342e55df6b00d7e865dd78e639a25b1
Hossam <ahmedalgamal789@yahoo.com>
2026-04-30 13:26:52 +0300

    dds_sequence item updated

--- Verification/Tx_Path_Verification/Tx_Path_env/Agents/DDS/dds_seq_item.py
+++ Verification/Tx_Path_Verification/Tx_Path_env/Agents/DDS/dds_seq_item.py
@@ -6,40 +6,31 @@ class dds_seq_item(uvm_sequence_item):
         super().__init__(name)
         # Hardware Inputs
         self.FTW_start = 0
-        self.cycles = 100
+        self.cycles = 40
         self.FTW_step = 0
         self.rst_n = 1
-        self.enable=0
+        self.enable = 0
+        
         # Output variables 
         self.final_amplitude = 0
-        self.valid_out=0
+        self.valid_out = 0
+        
+        # Streaming Metadata
+        self.sample_index = 0 # NEW: Tracks the current clock cycle of the stream
 
     def randomize(self):
         """Custom randomization mimicking SV constraints."""
-        # 32-bit Tuning Word Start (Full range)
         self.FTW_start = random.randint(0, (1 << 32) - 1)
-        
-        # 13-bit Cycles (Ensure it is at least 1 to prevent immediate resets)
-        #self.cycles = random.randint(1, (1 << 13) - 1)
-        
-        # 32-bit Step. Constrained to 20 bits to prevent aliasing 
-        # (stepping past the Nyquist limit in a single clock cycle).
         self.FTW_step = random.randint(0, (1 << 32) - 1)
-        # Reset can be either active (0) or inactive (1)
-        self.rst_n = random.choices([0, 1], weights=[5, 95])[0]  # Bias towards non-reset states for more meaningful tests
-        
+        self.rst_n = random.choices([0, 1], weights=[5, 95])[0]  
     
-    # STRING FORMATTING
     def convert2string(self):
-        """Full transaction printout (Inputs -> Outputs)"""
         return (f"{self.get_name()} "
                 f"IN[ rst_n={self.rst_n}, FTW_start={hex(self.FTW_start)}, "
                 f"FTW_step={hex(self.FTW_step)}, cycles={self.cycles} ] -> "
-                f"OUT[ final_amplitude={hex(self.final_amplitude)} ]"
-                )
+                f"OUT[ final_amplitude={hex(self.final_amplitude)} ]")
 
     def convert2string_stimulus(self):
-        """Input-only printout for the driver/sequencer logging"""
         return (f"{self.get_name()} "
                 f"STIMULUS: rst_n={self.rst_n}, FTW_start={hex(self.FTW_start)}, "
-                f"FTW_step={hex(self.FTW_step)}, cycles={self.cycles}")       
+                f"FTW_step={hex(self.FTW_step)}, cycles={self.cycles}")
