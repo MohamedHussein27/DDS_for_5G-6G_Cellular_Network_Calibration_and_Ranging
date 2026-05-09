48df813f46e7f212f1ee9366412b2feaf19f0f06
Mohamed Ahmed Mohamed Hussein <Mohamed_Hussein2100924@outlook.com>
2026-05-09 20:17:26 +0300

    update monitor in tx top agents

--- Verification/Tx_Path_Verification/Tx_Path_env/Agents/TOP/top_monitor.py
+++ Verification/Tx_Path_Verification/Tx_Path_env/Agents/TOP/top_monitor.py
@@ -14,27 +14,34 @@ class top_monitor(uvm_monitor):
         self.dut_mon= ConfigDB().get(self,"","DUT")
         
     async def run_phase(self):
+        await RisingEdge(self.dut_mon.clk)
         while True:
             await RisingEdge(self.dut_mon.clk)
             await ReadOnly()
             
-            seq_item = seq_item.create("top_item")
+            seq_item = top_item.create("seq_item")
             # here we will read the signals from the DUT and write them to the analysis port
-            seq_item.rst_n = self.dut_mon.rst_n.value
-            seq_item.dds_enable = self.dut_mon.dds_enable.value
-            seq_item.FTW_start = self.dut_mon.FTW_start.value
-            seq_item.cycles = self.dut_mon.cycles.value
-            seq_item.FTW_step = self.dut_mon.FTW_step.value
-            seq_item.ofdm_rd_en = self.dut_mon.ofdm_rd_en.value
-            seq_item.ofdm_in_real=self.dut_mon.ofdm_in_real.value
-            seq_item.ofdm_in_imag=self.dut_mon.ofdm_in_imag.value
+            seq_item.rst_n          = int(self.dut_mon.rst_n.value)  
+            seq_item.dds_enable     = int(self.dut_mon.dds_enable.value)
+            seq_item.FTW_start      = int(self.dut_mon.FTW_start.value)
+            seq_item.cycles         = int(self.dut_mon.cycles.value)
+            seq_item.FTW_step       = int(self.dut_mon.FTW_step.value)
+
+
+            #seq_item.ofdm_in_real   = self.dut_mon.ofdm_in_re.value
+            #seq_item.ofdm_in_imag   = self.dut_mon.ofdm_in_im.value
+
             # capturing the IFFT outputs 
-            seq_item.tx_valid=self.dut_mon.tx_valid.value
-            seq_item.tx_out_real=self.dut_mon.tx_out_real.value
-            seq_item.tx_out_imag=self.dut_mon.tx_out_imag.value
+            if self.dut_mon.tx_valid.value == 1:
+                seq_item.tx_valid       = int(self.dut_mon.tx_valid.value)
+                seq_item.tx_out_real    = int(self.dut_mon.tx_out_re.value)
+                seq_item.tx_out_imag    = int(self.dut_mon.tx_out_im.value)
+            else:
+                seq_item.tx_valid       = 0
+                seq_item.tx_out_real    = 0
+                seq_item.tx_out_imag    = 0
             
-            
-            
-            
+            # monitoring the top
+            self.logger.info(f"Monitoring top: rst_n={seq_item.rst_n}, dds_enable={seq_item.dds_enable}, FTW_start={seq_item.FTW_start}, cycles={int(seq_item.cycles)}, FTW_step={seq_item.FTW_step}, tx_valid={seq_item.tx_valid}, tx_out_real={seq_item.tx_out_real}, tx_out_imag={seq_item.tx_out_imag}")
             self.mon_ap.write(seq_item)
             
