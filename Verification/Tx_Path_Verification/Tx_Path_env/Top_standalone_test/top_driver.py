b41e2446c2560e203f0b586461a38ff87b2d0dfd
Mohamed Ahmed Mohamed Hussein <Mohamed_Hussein2100924@outlook.com>
2026-05-11 08:52:42 +0300

    updates in the tx env ( add the reg file to the tx top, modified the seq item, driver, monitor to match the new inputs) TODO: i will connect the DDS monitor with the scoreboard as to be able to pass its params to the golden model, i will use dut operator to access the OFDM ROM for firther tests

--- Verification/Tx_Path_Verification/Tx_Path_env/Top_standalone_test/top_driver.py
+++ Verification/Tx_Path_Verification/Tx_Path_env/Top_standalone_test/top_driver.py
@@ -1,6 +1,3 @@
-"""
-
-"""
 import pyuvm
 from pyuvm import *
 from top_seq_item import *
@@ -20,20 +17,19 @@ class top_driver(uvm_driver):
             # 2. Synchronize with the clock edge before driving the signals
             await FallingEdge(self.dut_drv.clk)
             
-            # 3. Drive the stimulus into the dut_drv inputs
-            self.dut_drv.rst_n.value = seq_item.rst_n
-            self.dut_drv.dds_enable.value = seq_item.dds_enable
-            self.dut_drv.FTW_start.value = seq_item.FTW_start
-            self.dut_drv.cycles.value = seq_item.cycles
-            self.dut_drv.FTW_step.value = seq_item.FTW_step
-
-            #self.dut_drv.ofdm_in_re.value = seq_item.ofdm_in_real
-            #self.dut_drv.ofdm_in_im.value = seq_item.ofdm_in_imag
+            # 3. Drive the stimulus into the memory-mapped bus inputs
+            self.dut_drv.rst_n.value   = seq_item.rst_n
+            self.dut_drv.addr.value    = seq_item.addr
+            self.dut_drv.wr_en.value   = seq_item.wr_en
+            self.dut_drv.wr_data.value = seq_item.wr_data
+            self.dut_drv.rd_en.value   = seq_item.rd_en
 
             # printing the driven values for debugging
             self.logger.info(
-                f"=================================================================================================================================================="
-                f"Driving: rst_n={seq_item.rst_n}, dds_enable={seq_item.dds_enable}, FTW_start={seq_item.FTW_start}, cycles={seq_item.cycles}, FTW_step={seq_item.FTW_step}")
+                f"====================================================================================================\n"
+                f"Driving BUS: rst_n={seq_item.rst_n}, addr={hex(seq_item.addr)}, we={seq_item.wr_en}, "
+                f"wdata={hex(seq_item.wr_data)}, re={seq_item.rd_en}"
+            )
             
             # 4. Notify the sequencer that this transaction is complete
             self.seq_item_port.item_done()
--- Verification/Tx_Path_Verification/Tx_Path_env/Top_standalone_test/top_monitor.py
+++ Verification/Tx_Path_Verification/Tx_Path_env/Top_standalone_test/top_monitor.py
@@ -1,17 +1,14 @@
-"""
- 
-"""
-
 import pyuvm
 from pyuvm import *
 from top_seq_item import *
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import *
+
 class top_monitor(uvm_monitor):
     def build_phase(self):
         self.mon_ap = uvm_analysis_port("mon_ap", self)
-        self.dut_mon= ConfigDB().get(self,"","DUT")
+        self.dut_mon = ConfigDB().get(self, "", "DUT")
         
     async def run_phase(self):
         await RisingEdge(self.dut_mon.clk)
@@ -20,28 +17,42 @@ class top_monitor(uvm_monitor):
             await ReadOnly()
             
             seq_item = top_item.create("seq_item")
-            # here we will read the signals from the DUT and write them to the analysis port
-            seq_item.rst_n          = int(self.dut_mon.rst_n.value)  
-            seq_item.dds_enable     = int(self.dut_mon.dds_enable.value)
-            seq_item.FTW_start      = int(self.dut_mon.FTW_start.value)
-            seq_item.cycles         = int(self.dut_mon.cycles.value)
-            seq_item.FTW_step       = int(self.dut_mon.FTW_step.value)
+            
+            # 1. Capture the Memory-Mapped Bus Inputs
+            seq_item.rst_n   = int(self.dut_mon.rst_n.value)  
+            seq_item.addr    = int(self.dut_mon.addr.value)
+            seq_item.wr_en   = int(self.dut_mon.wr_en.value)
+            seq_item.wr_data = int(self.dut_mon.wr_data.value)
+            seq_item.rd_en   = int(self.dut_mon.rd_en.value)
+            
+            # 2. Capture the Bus Outputs (Read Data & Hardware Flags)
+            # Using try/except in case rd_data or dds_ready_flag are 'X' or 'Z' during reset
+            try:
+                seq_item.rd_data = int(self.dut_mon.rd_data.value)
+            except ValueError:
+                seq_item.rd_data = 0
+                
+            try:
+                seq_item.dds_ready_flag = int(self.dut_mon.dds_ready_flag.value)
+            except ValueError:
+                seq_item.dds_ready_flag = 0
 
-
-            #seq_item.ofdm_in_real   = self.dut_mon.ofdm_in_re.value
-            #seq_item.ofdm_in_imag   = self.dut_mon.ofdm_in_im.value
-
-            # capturing the IFFT outputs 
+            # 3. Capture the TX Datapath Outputs 
             if self.dut_mon.tx_valid.value == 1:
-                seq_item.tx_valid       = int(self.dut_mon.tx_valid.value)
-                seq_item.tx_out_real    = int(self.dut_mon.tx_out_re.value)
-                seq_item.tx_out_imag    = int(self.dut_mon.tx_out_im.value)
+                seq_item.tx_valid    = 1
+                seq_item.tx_out_real = int(self.dut_mon.tx_out_re.value)
+                seq_item.tx_out_imag = int(self.dut_mon.tx_out_im.value)
             else:
-                seq_item.tx_valid       = 0
-                seq_item.tx_out_real    = 0
-                seq_item.tx_out_imag    = 0
+                seq_item.tx_valid    = 0
+                seq_item.tx_out_real = 0
+                seq_item.tx_out_imag = 0
             
-            # monitoring the top
-            self.logger.info(f"Monitoring top: rst_n={seq_item.rst_n}, dds_enable={seq_item.dds_enable}, FTW_start={seq_item.FTW_start}, cycles={int(seq_item.cycles)}, FTW_step={seq_item.FTW_step}, tx_valid={seq_item.tx_valid}, tx_out_real={seq_item.tx_out_real}, tx_out_imag={seq_item.tx_out_imag}")
+            # 4. Log and Send to Analysis Port
+            self.logger.info(
+                f"Monitoring BUS: rst={seq_item.rst_n}, addr={hex(seq_item.addr)}, we={seq_item.wr_en}, "
+                f"wdata={hex(seq_item.wr_data)}, re={seq_item.rd_en} | "
+                f"OUT: rdata={hex(seq_item.rd_data)}, ready={seq_item.dds_ready_flag}, "
+                f"tx_valid={seq_item.tx_valid}"
+            )
+            
             self.mon_ap.write(seq_item)
-            
